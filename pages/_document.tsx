583284356413025341c1c34af99f61be88c01c0d
Xetera <mythologyali@gmail.com>
2019-06-11 08:18:38 -0700

    styles now load properly

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, { Main, NextScript, Head } from "next/document";
+import { ServerStyleSheets } from "@material-ui/styles";
+import * as React from "react";
+
+class Html extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head/>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+  static async getInitialProps(ctx: any) {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: any) => (props: any) =>
+          sheets.collect(<App {...props} />)
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: <>{sheets.getStyleElement()}</>
+    };
+  }
+}
+
+export default Html;
